3d6f37dc8a1d8637c0b345c02e2f8001fe65a186
ciretorres <erictorres.velasco@gmail.com>
2025-03-17 13:49:17 -0600

    feat: heredando EventEmmiter a la class Search y agregando método searchCount

--- Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/search.js
+++ Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/search.js
@@ -1,2 +1,11 @@
 const API = require('./mock-api');
 // To count the matches, call API.countMatches(term) where term is the search term
+
+const EventEmitter = require('events');
+
+module.exports = class Search extends EventEmitter {
+    constructor() {
+        super()
+    }
+    searchCount(searchTerm) { }
+}
