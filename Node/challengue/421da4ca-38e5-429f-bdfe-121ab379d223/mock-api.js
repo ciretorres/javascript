ca3bf596e9300aeba87ee1d6f5fd019830a696ba
ciretorres <erictorres.velasco@gmail.com>
2025-03-18 11:14:56 -0600

    feat: emit the event SEARCH_ERROR if term passed to the function is false

--- Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/mock-api.js
+++ Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/mock-api.js
@@ -4,12 +4,7 @@ const countMatches = (searchTerm) => {
             const value = Math.floor(Math.random() * 100) + 1;
 
             if (searchTerm !== 'error') {
-                if (searchTerm === undefined) {
-                    reject(new Error('INVALID_TERM'))    
-                } else {                 
                 resolve(value);   
-                }
-                // resolve(value);   
             } else {
                 reject(new Error('CONNECTION_ERROR'))
             }
--- Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/search.js
+++ Node/challengue/421da4ca-38e5-429f-bdfe-121ab379d223/search.js
@@ -8,31 +8,23 @@ module.exports = class Search extends EventEmitter {
         super()
     }
     searchCount(searchTerm) {
-        this.emit('SEARCH_STARTED', searchTerm);
-        // API.countMatches(searchTerm)
-        //     .then(
-        //     // resolve param
-        //     (value) => {
-        //         this.emit('SEARCH_SUCCESS', { term: searchTerm, count: value });
-        //     });
+        
+        if (!searchTerm) {
+            this.emit('SEARCH_ERROR', { term: searchTerm, message: new Error('INVALID_TERM').message })
+        } else {
+            this.emit('SEARCH_STARTED', searchTerm);
 
-        API.countMatches(searchTerm)
+            API.countMatches(searchTerm)
             .then(
             // resolve param
             (value) => {
                 this.emit('SEARCH_SUCCESS', { term: searchTerm, count: value });
             }, 
             // error param
-            (value) => {
-                // if (searchTerm == false) {
-                //     console.log('indefinidoooooooooo')
-                //     this.emit('SEARCH_ERROR', { term: searchTerm, message: value })
-                // }
-                // console.log('searchTerm', searchTerm)
+            (value) => {                
                 value = value.message
-                // this.emit('SEARCH_ERROR', { term: searchTerm, message: 'CONNECTION_ERROR' })
                 this.emit('SEARCH_ERROR', { term: searchTerm, message: value })
             });
-            
+        }   
      }
 }
